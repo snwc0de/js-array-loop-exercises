d50d1965040cf7806208079444c69e2c848e97cd
snwc0de <154846385+snwc0de@users.noreply.github.com>
2025-01-01 18:46:22 +0100

    Completed excercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(one, two) {
+  return one + two;
 };
 
-const subtract = function() {
-	
+const subtract = function(one, two) {
+	return one - two;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function() {
-	
+const power = function(one, two) {
+	return one ** two;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+	for (let i = 1; i <= num; i++) {
+    sum *= i;
+  }
+  return sum
 };
 
 // Do not edit below this line
